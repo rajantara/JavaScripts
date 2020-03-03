1e9300f556b1408c25395b875c58d6e6f94db731
rajantara <rajanpensas@gmail.com>
2020-03-03 18:57:59 +0700

    C7 update catatan

--- C7-weirdMultiply/c7.js
+++ C7-weirdMultiply/c7.js
@@ -1,13 +1,13 @@
 
 function WeirdMultiply(sentences) {
   let x = sentences.toString()
-  let z = x.split("")
+  let z = x.split("")// pada tahap ini x.split bfungsi untuk memisahkan,atau mengihalkan salah satu yang tidak dibutuhkan 
   let y = 1
-  if (z.length === 1) {
-    return z;
-
-  } else {
-    for (var i = 0; i < z.length; i++) {
+  if (z.length === 1) {// pada tahap ini panjang  "z" akan di hitung pada array ksong
+    return z;// di sini akan di return oleh z pada z lengt di sebelumnya
+s
+  } else {// jika hasilnya salah maka hasil alah itu akan di shering disini
+    for (var i = 0; i < z.length; i++) {// dan pada tahap ini akn dilooping oleh for
       y *= z[i];
 
     }
