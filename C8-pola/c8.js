76429c5cd8281669b16b07438007de291b9da880
rajantara <rajanpensas@gmail.com>
2020-03-03 19:01:18 +0700

    update c8

--- C8-pola/c8.js
+++ C8-pola/c8.js
@@ -1,10 +1,10 @@
 function Pola(str) {
-    const hasil = []; // untuk array kosong simpen hasil 
-    let potonganPola = str.split(' '); // di split pakai spasi 
+    const hasil = []; // kurung siku untuk array kosong simpen hasil 
+    let potonganPola = str.split(' '); // di split pakai spasi berfunsgi memisahkan angka atau tidak memangio angka yang tdk dibutukan 
 
     for (let a = 0; a <= 9; a++) {
         for (let b = 0; b <= 9; b++) {
-            if (potonganPola[0].replace('#', a) * potonganPola[2] == potonganPola[4].replace('#', b)) {
+            if (potonganPola[0].replace('#', a) * potonganPola[2] == potonganPola[4].replace('#', b)) {// funsgi replace adlah mencari string utunk nilai yang ditentukan oleh expresi reguler dan mengebalikan string baru dimana nilai ditentukan diganti
                 hasil.push(a, b)
 
             }
