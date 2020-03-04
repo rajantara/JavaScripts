2a410fdac2d6a824f127c0e788623b76a4588789
rajantara <rajanpensas@gmail.com>
2020-03-04 09:23:37 +0700

    update c1

--- C1-sum/c1.js
+++ C1-sum/c1.js
@@ -2,7 +2,7 @@ function sum() {// fungsi sum adalah nilai dena di berikan parameter kosong untu
     var total = 0// di tahap ini variabel totalnya mulai dari "0" but sometime looping membutukan varibel ini untuk di jumhlakn bersama arguements
     for (let i = 0; i < arguments.length; i++) {//pada tahap ini arguemnts lebih kecil dari "i" dan agruemnets menghitung panjang mengunkan lenght dan di tambah mengunakan i++
         total = total + arguments[i];// disni total akan di jumlakan dengan arguemnts bersama i 
-    }
+    }   //total akan di tangkap dari var tototal dan akan di tambah arguments varibael index
     console.log(total);// dan hasil semua akan di tangkap oleh console log total 
 }
 sum(1, 2, 7);
@@ -10,20 +10,21 @@ sum(1, 4)
 sum(11)
 sum(10, 3, 6, 7, 9);
 
-//kata kunci
-//ingat for adalah looping 
-//>sum adalah nilai
-//>pada tahap loooping apakah i lebih kecil dari agruemnts? maka di hitung panjangnya guys
-
 
 /*
 catatan penting :
+
+kata kunci
+ingat for adalah looping 
+>sum adalah nilai
+>pada tahap loooping apakah i lebih kecil dari agruemnts? maka di hitung panjangnya guys
+
 
 1.Variabel tidak diinisialisasi
 Ketika variabel JavaScript dideklarasikan, mereka memiliki nilai awal yang tidak terdefinisi. Jika Anda melakukan operasi matematika pada variabel yang tidak ditentukan, hasil Anda akan menjadi NaN yang berarti "Bukan Angka". Jika Anda menggabungkan string dengan variabel tidak terdefinisi, Anda akan mendapatkan string literal "tidak terdefinisi".
 
 
 
-cd .. out folder
+2.cd .. out folder berfungsi untuk keluar dari folder sebeleumnya
 
 */
